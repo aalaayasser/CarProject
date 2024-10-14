e72262cf9484fdf65dcde05ca0e4960d150fd4e6
elbastamy <mohamedelbostamy62@gmail.com>
2024-10-14 20:48:30 +0300

    create color , part classes

---
+++ DALProject/Data/Configartions/ColorConfigartion.cs
@@ -0,0 +1,26 @@
+﻿using DALProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALProject.Data.Configartions
+{
+    public class ColorConfiguration : IEntityTypeConfiguration<Color>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Color> builder)
+        {
+            builder.Property(e => e.ColorId)
+           .IsRequired() 
+           .ValueGeneratedOnAdd(); 
+
+        
+            builder.Property(e => e.ColorName)
+                .IsRequired()
+                .HasMaxLength(50) 
+                .HasColumnType("varchar"); 
+        }
+    }
+}
--- DALProject/obj/Debug/net5.0/DALProject.AssemblyInfo.cs
+++ DALProject/obj/Debug/net5.0/DALProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DALProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dab3e2ad53dc58df49eac03eec32133df937508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd8ab13d1387e7c129815dd74c9229e86bc8560")]
 [assembly: System.Reflection.AssemblyProductAttribute("DALProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DALProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
