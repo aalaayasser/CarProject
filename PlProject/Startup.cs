db58cdde1952e246010a51716492111efffa112a
Amg04 <130388192+Amg04@users.noreply.github.com>
2024-10-14 17:30:30 +0300

    changs

--- PlProject/Startup.cs
+++ PlProject/Startup.cs
@@ -1,6 +1,8 @@
+using DALProject;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,15 @@ namespace PlProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<CarAppDbContext>(
+           optionsBuilder => {
+               optionsBuilder
+               .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+           }
+
+          );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
